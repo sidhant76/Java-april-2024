2fe5b85e1f748931f79c45dee6c4e8933533bb59
sidhant76 <150133951+sidhant76@users.noreply.github.com>
2024-04-16 11:50:13 +0530

    String and StringBuffer methods

---
+++ Sbuffer.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class Sbuffer{
+  public static void main (String args[])
+{
+ // StringBuffer sb1 = new StringBuffer("hiii");
+ // sb1.append("sid");
+ // sb1.insert(3,"dd");
+
+ // System.out.println(sb1.capacity());
+ // System.out.println(sb1.length());
+
+ StringBuffer sb1 = new StringBuffer("hiii");
+
+  System.out.println(sb1.capacity());
+
+
+
+
+
+
+
+  System.out.println(sb1);
+}
+}
